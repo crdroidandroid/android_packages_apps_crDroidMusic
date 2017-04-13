82c97abb211c4734eab2e3b0332fbb2a8265a16f
kantjer <kantjer@gmail.com>
2017-04-13 08:26:13 +0200

    add build script and tools the script will build, sign and zipalign the apk
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+ROOT_PATH=$PWD
+BUILD_PATH="$ROOT_PATH/app/build/outputs/apk/"
+APP_NAME="Phonograph"
+
+# Build app
+./gradlew build
+
+#sign and zipalign
+java -jar sign.jar $BUILD_PATH/app-release-unsigned.apk
+zipalign -f -v 4 $BUILD_PATH/app-release-unsigned.s.apk $ROOT_PATH/$APP_NAME-release.apk
+
+# cleanup
+./gradlew clean
