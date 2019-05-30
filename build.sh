a3269dcbf45e1802c0050fad235b6e6674b331b8
Pranav Vashi <neobuddy89@gmail.com>
2019-05-30 23:43:17 +0530

    Update build gradle and build script
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ BUILD_PATH="$ROOT_PATH/app/build/outputs/apk/release"
 APP_NAME="crDroidMusic"
 
 # Build app
-./gradlew assembleRelease
+./gradlew assembleRelease --info
 
 #sign and zipalign
-java -jar sign.jar $BUILD_PATH/app-release-unsigned.apk
-$ROOT_PATH/zipalign -f -v 4 $BUILD_PATH/app-release-unsigned.s.apk $ROOT_PATH/$APP_NAME.apk
+#java -jar sign.jar $BUILD_PATH/app-release-unsigned.apk
+$ROOT_PATH/zipalign -f -v 4 $BUILD_PATH/app-release-unsigned.apk $ROOT_PATH/$APP_NAME.apk
 
 # cleanup
 ./gradlew clean
